ab0eedc46e5b3c998f67cf1a9c08b5f66688c3dd
b2300733 <b2300733@helplive.edu.my>
2025-09-17 13:06:37 +0800

    added Sidebar (navigation) with sample

---
+++ Sidebar/sidebar.php
@@ -0,0 +1,20 @@
+<div class="bg-dark text-white d-flex flex-column" style="width: 300px; height: 100vh;">
+    <div class="d-flex justify-content-between align-items-center p-3 border-bottom border-secondary">
+        <i data-lucide="circle-user-round"></i>
+        <i data-lucide="arrow-left-to-line"></i>
+    </div>
+
+    <div class="p-3 fs-4 fw-medium d-flex flex-column flex-grow-1 overflow-auto">
+        <div class="mb-auto">
+            <a href="#" class="nav-link text-light py-2">Food Inventory</a>
+            <a href="#" class="nav-link text-light py-2">Browse Food Items</a>
+            <a href="#" class="nav-link text-light py-2">Plan Weekly Meals</a>
+            <a href="#" class="nav-link text-light py-2">Track and Report</a>
+            <a href="#" class="nav-link text-light py-2">Notifications</a>
+        </div>
+        <div>
+            <a href="#" class="nav-link text-light py-2">Settings</a>
+            <button class="btn text-light m-0 p-0 py-2 fs-4 fw-medium text-start">Logout</button>
+        </div>
+    </div>
+</div>
