4ea7964fa17bc2c23e0c37c4d31214329568dbd0
AaronT0219 <kianpoh0219@gmail.com>
2025-10-06 00:11:31 +0800

    added favicon

--- templates/base.php
+++ templates/base.php
@@ -17,6 +17,7 @@ if (!isset($_SESSION['email'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SaveBite</title>
 
+    <link rel="icon" type="image/x-icon" href="../assets/images/icon_pure.png">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="base.css">
     <script src="https://unpkg.com/lucide@latest"></script>
