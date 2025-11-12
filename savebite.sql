a55ff6dff97c91bfc9a295098c576e1003c49411
QUJIAWEI-SS <b2400595@helplive.edu.my>
2025-11-12 17:20:02 +0800

    Change some details of SQL

--- savebite.sql
+++ savebite.sql
@@ -3,15 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
--- Generation Time: Oct 15, 2025 at 06:42 AM
-=======
 -- Generation Time: Nov 12, 2025 at 09:44 AM
->>>>>>> Stashed changes
-=======
--- Generation Time: Nov 12, 2025 at 09:44 AM
->>>>>>> Stashed changes
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
@@ -55,18 +47,11 @@ CREATE TABLE `donation` (
 INSERT INTO `donation` (`donation_id`, `status`, `category`, `pickup_location`, `description`, `donation_date`, `donor_user_id`, `claimant_user_id`, `availability`, `contact`) VALUES
 (47, 'pending', 'Produce', '', NULL, '2025-10-15', 10, NULL, '', ''),
 (48, 'pending', 'Produce', '', NULL, '2025-10-15', 10, NULL, '', ''),
-<<<<<<< Updated upstream
-(49, 'pending', NULL, 'ddd', NULL, '2025-10-15', 10, NULL, NULL, NULL);
-=======
 (49, 'pending', NULL, 'ddd', NULL, '2025-10-15', 10, NULL, NULL, NULL),
 (51, 'pending', NULL, 'midvalley', NULL, '2025-10-22', 11, NULL, NULL, NULL),
 (52, 'pending', NULL, 'midvalley', NULL, '2025-10-30', 11, NULL, NULL, NULL),
 (54, 'pending', NULL, 'city kepong', NULL, '2025-11-01', 11, NULL, NULL, NULL),
 (57, 'picked_up', 'Produce', 'park', '11', '2025-12-12', 8, NULL, 'yes', '60 1919191919');
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
 
 -- --------------------------------------------------------
 
@@ -87,16 +72,9 @@ CREATE TABLE `donation_fooditem` (
 INSERT INTO `donation_fooditem` (`donation_id`, `fooditem_id`, `quantity`) VALUES
 (47, 43, 1),
 (48, 44, 5),
-<<<<<<< Updated upstream
-(49, 43, 1);
-=======
 (49, 43, 1),
 (52, 48, 5),
 (57, 81, 1);
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
 
 -- --------------------------------------------------------
 
@@ -112,38 +90,15 @@ CREATE TABLE `fooditem` (
   `expiry_date` date NOT NULL DEFAULT current_timestamp(),
   `storage_location` enum('Fridge','Freezer','Pantry','Countertop') NOT NULL,
   `description` varchar(80) DEFAULT NULL,
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-  `status` enum('available','used','expired','donation') NOT NULL,
-  `user_id` int(20) UNSIGNED NOT NULL
-=======
   `status` enum('available','used','expired','donation','reserved') NOT NULL,
   `user_id` int(20) UNSIGNED NOT NULL,
   `created_at` date DEFAULT NULL
->>>>>>> Stashed changes
-=======
-  `status` enum('available','used','expired','donation','reserved') NOT NULL,
-  `user_id` int(20) UNSIGNED NOT NULL,
-  `created_at` date DEFAULT NULL
->>>>>>> Stashed changes
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `fooditem`
 --
 
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-INSERT INTO `fooditem` (`foodItem_id`, `category`, `food_name`, `quantity`, `expiry_date`, `storage_location`, `description`, `status`, `user_id`) VALUES
-(38, 'Protein', 'apple', 0, '2025-12-12', 'Pantry', '111', 'used', 8),
-(40, 'Produce', '1', 1, '1111-11-11', 'Fridge', '1', 'available', 8),
-(41, 'Produce', '1', 1, '1111-11-11', 'Fridge', '1', 'available', 8),
-(42, 'Produce', '34', 44, '2025-10-24', 'Fridge', '', 'used', 9),
-(43, 'Produce', '1', 1, '1111-11-11', 'Fridge', '11', 'donation', 10),
-(44, 'Produce', '55', 5, '2222-05-05', 'Fridge', '55', 'available', 10);
-=======
-=======
->>>>>>> Stashed changes
 INSERT INTO `fooditem` (`foodItem_id`, `category`, `food_name`, `quantity`, `expiry_date`, `storage_location`, `description`, `status`, `user_id`, `created_at`) VALUES
 (42, 'Produce', '34', 44, '2025-10-24', 'Fridge', '', 'used', 9, '2025-11-12'),
 (43, 'Produce', '1', 1, '1111-11-11', 'Fridge', '11', 'donation', 10, '2025-11-12'),
@@ -163,10 +118,6 @@ INSERT INTO `fooditem` (`foodItem_id`, `category`, `food_name`, `quantity`, `exp
 (78, 'Produce', 'Banana', 1, '2025-11-15', 'Fridge', '', 'reserved', 11, '2025-11-12'),
 (79, 'Dairy & Bakery', 'Cookie', 1, '2025-10-31', 'Pantry', 'delicious cookies :)', 'reserved', 11, '2025-11-12'),
 (81, 'Produce', 'apple', 1, '2025-11-12', 'Fridge', '', 'used', 8, NULL);
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
 
 -- --------------------------------------------------------
 
@@ -180,7 +131,7 @@ CREATE TABLE `food_items` (
 ,`quantity` int(10)
 ,`category` enum('Produce','Protein','Dairy & Bakery','Grains & Pantry','Snacks & Beverages')
 ,`expiry_date` date
-,`status` enum('available','used','expired','donation')
+,`status` enum('available','used','expired','donation','reserved')
 ,`description` varchar(80)
 ,`created_by` int(20) unsigned
 ,`storage_location` enum('Fridge','Freezer','Pantry','Countertop')
@@ -202,6 +153,15 @@ CREATE TABLE `mealplan` (
   `user_id` int(20) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `mealplan`
+--
+
+INSERT INTO `mealplan` (`mealplan_id`, `meal_name`, `mealplan_date`, `meal_type`, `description`, `user_id`) VALUES
+(12, 'Burger MealPlan', '2025-11-15', 'lunch', 'Delicious tasting Burger, with amazing and fresh ingredients!', 11),
+(15, 'Fruit Salad', '2025-11-15', 'breakfast', 'Delicious tasting Burger, with amazing and fresh ingredients!', 11),
+(23, 'Cookies Fruit Salad', '2025-11-12', 'breakfast', '', 11);
+
 -- --------------------------------------------------------
 
 --
@@ -213,6 +173,19 @@ CREATE TABLE `mealplan_fooditem` (
   `fooditem_id` int(20) NOT NULL,
   `quantity` int(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `mealplan_fooditem`
+--
+
+INSERT INTO `mealplan_fooditem` (`mealplan_id`, `fooditem_id`, `quantity`) VALUES
+(12, 53, 2),
+(15, 61, 1),
+(15, 62, 1),
+(15, 63, 10),
+(23, 77, 1),
+(23, 78, 1),
+(23, 79, 1);
 
 -- --------------------------------------------------------
 
@@ -231,8 +204,6 @@ CREATE TABLE `notification` (
   `user_id` int(20) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-<<<<<<< Updated upstream
-=======
 --
 -- Dumping data for table `notification`
 --
@@ -250,7 +221,6 @@ INSERT INTO `notification` (`notification_id`, `title`, `description`, `notifica
 (54, 'Donation created', 'You donated \"\" at ', '2025-11-12 16:36:53', 'seen', 'donation', 57, 8),
 (55, 'Donation picked up', 'apple picked up (Donated on: 2025-12-12)', '2025-11-12 16:38:07', 'seen', 'donation', 57, 8);
 
->>>>>>> Stashed changes
 -- --------------------------------------------------------
 
 --
@@ -271,10 +241,10 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`user_id`, `user_name`, `email`, `password`, `household_number`, `isAuthActive`) VALUES
-(7, 'Aaron', 'brabrab@gmail.com', '$2y$10$ONo/hJPFgUFKsiIbGvSEg.QFuOagb0J7nwppCxN2FGiepRtzLLjEK', 3, 0),
 (8, 'QuJiaWei', 'b2400595@helplive.edu.my', '$2y$10$oXBwXpnGZXsGyVOcTaS35uZn1H.51V31s17NwJ9ahs9bt.b49lIhq', 0, 0),
 (9, 'Qu JiaWei', 'b2300733@helplive.edu.my', '$2y$10$K0b7t1yNqheIBfUAcBfXsOF2EAqO5ja2CtowF4/IaVturAdNuG0AC', 0, 0),
-(10, 'jia', 'nazermy.qu@gmail.com', '$2y$10$t0kDtd4R8SG8ApmX9S8Bv./YGG7IKbXvvO5cxA45RjS8HSvVBaytK', 0, 1);
+(10, 'jia', 'nazermy.qu@gmail.com', '$2y$10$t0kDtd4R8SG8ApmX9S8Bv./YGG7IKbXvvO5cxA45RjS8HSvVBaytK', 0, 1),
+(11, 'Aaron', 'kianpoh0219@gmail.com', '$2y$10$SthVD4qsV7Hj9DFszANEoOEQulbwoxfK2QFB3owAjTdTdCMOyh9D.', 69, 0);
 
 -- --------------------------------------------------------
 
@@ -379,55 +349,31 @@ ALTER TABLE `verification_codes`
 -- AUTO_INCREMENT for table `donation`
 --
 ALTER TABLE `donation`
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-  MODIFY `donation_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=50;
-=======
   MODIFY `donation_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=58;
->>>>>>> Stashed changes
-=======
-  MODIFY `donation_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=58;
->>>>>>> Stashed changes
 
 --
 -- AUTO_INCREMENT for table `fooditem`
 --
 ALTER TABLE `fooditem`
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-  MODIFY `foodItem_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
-=======
   MODIFY `foodItem_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=82;
->>>>>>> Stashed changes
-=======
-  MODIFY `foodItem_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=82;
->>>>>>> Stashed changes
 
 --
 -- AUTO_INCREMENT for table `mealplan`
 --
 ALTER TABLE `mealplan`
-  MODIFY `mealplan_id` int(20) NOT NULL AUTO_INCREMENT;
+  MODIFY `mealplan_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
 -- AUTO_INCREMENT for table `notification`
 --
 ALTER TABLE `notification`
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-  MODIFY `notification_id` int(20) NOT NULL AUTO_INCREMENT;
-=======
   MODIFY `notification_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=56;
->>>>>>> Stashed changes
-=======
-  MODIFY `notification_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=56;
->>>>>>> Stashed changes
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `user_id` int(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `user_id` int(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `verification_codes`
@@ -469,8 +415,8 @@ ALTER TABLE `mealplan`
 -- Constraints for table `mealplan_fooditem`
 --
 ALTER TABLE `mealplan_fooditem`
-  ADD CONSTRAINT `fooditem_mealplan_fk` FOREIGN KEY (`fooditem_id`) REFERENCES `fooditem` (`foodItem_id`),
-  ADD CONSTRAINT `mealplan_fooditem_fk` FOREIGN KEY (`mealplan_id`) REFERENCES `mealplan` (`mealplan_id`);
+  ADD CONSTRAINT `fooditem_mealplan_fk` FOREIGN KEY (`fooditem_id`) REFERENCES `fooditem` (`foodItem_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `mealplan_fooditem_fk` FOREIGN KEY (`mealplan_id`) REFERENCES `mealplan` (`mealplan_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `notification`
